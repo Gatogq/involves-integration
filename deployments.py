cb19a2287bc7ab74bd382b848fee941bdfbe91d8
Gatogq <speedyscomin@ciencias.unam.mx>
2024-05-31 13:12:22 -0600

    just removed a typo on deployments

--- deployments.py
+++ deployments.py
@@ -8,6 +8,5 @@ if __name__ == '__main__':
 
                         ).deploy(
                             name='update_involves_db_from_repo',
-                            work_pool_name='Involves',
-                            cron=""
+                            work_pool_name='Involves'
                         )
